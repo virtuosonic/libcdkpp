c2db4e99b2013cbc1e7aebf4cd7c960405ae2364
Gabriel Espinoza <virtuoso.sdc@gmail.com>
2023-07-20 17:22:32 -0700

    identation

--- cdk.hpp
+++ cdk.hpp
@@ -16,7 +16,8 @@ License:        MIT
 #include <algorithm>
 #include <functional>
 
-namespace cdk {
+namespace cdk
+{
 
 
 auto string2charptr = [](const std::string_view s)
@@ -28,9 +29,9 @@ using StringList = std::vector<std::string_view>;
 
 std::vector<char*> transformStringList(const StringList &v)
 {
-    std::vector<char*> vc;
-    std::transform(v.begin(),v.end(),back_inserter(vc),string2charptr);
-    return vc;
+	std::vector<char*> vc;
+	std::transform(v.begin(),v.end(),back_inserter(vc),string2charptr);
+	return vc;
 }
 
 struct widget
@@ -41,52 +42,60 @@ struct widget
 /**
  * Screen object that manages its child widgets.
 */
-class screen {
-	struct deleter{
-		void operator ()(CDKSCREEN* p){destroyCDKScreen(p);}
+class screen
+{
+	struct deleter
+	{
+		void operator ()(CDKSCREEN* p)
+		{
+			destroyCDKScreen(p);
+		}
 	};
 
 	using screenptr = std::unique_ptr<CDKSCREEN,deleter>;
-    screenptr _ptr;
+	screenptr _ptr;
 	//static bool atexit_installed{false};
-    friend class label;
+	friend class label;
 	friend class button;
 public:
-    /**
-     * Constructor.
-     * Creates an object in a curses WINDOW,
-     * you can pass initscr() to use the full
-     * terminal. It calls initCDKScreen and 
-     * install an atexit handler
-    */
-    screen(WINDOW *cursesWindow){
+	/**
+	 * Constructor.
+	 * Creates an object in a curses WINDOW,
+	 * you can pass initscr() to use the full
+	 * terminal. It calls initCDKScreen and
+	 * install an atexit handler
+	*/
+	screen(WINDOW *cursesWindow)
+	{
 		_ptr = screenptr(initCDKScreen(cursesWindow));
-        initCDKColor();
+		initCDKColor();
 		//if (!atexit_installed){
-		    //atexit(endCDK);
+		//atexit(endCDK);
 		//}
-    }
+	}
 	~screen()
 	{
 		endCDK();
 	}
-    /**
-     * Erase screen.
-     * Erases all of the widgets which 
-     * are currently associated to the given screen.  
-     * This does NOT destroy the widgets.
-    */
-    void erase() { 
-        eraseCDKScreen(_ptr.get());
-    }
-    /**
-     * Redraws all of the widgets 
-     * which are currently associated 
-     * to this screenobject.
-    */
-    void refresh() {
-        refreshCDKScreen(_ptr.get());
-    }
+	/**
+	 * Erase screen.
+	 * Erases all of the widgets which
+	 * are currently associated to the given screen.
+	 * This does NOT destroy the widgets.
+	*/
+	void erase()
+	{
+		eraseCDKScreen(_ptr.get());
+	}
+	/**
+	 * Redraws all of the widgets
+	 * which are currently associated
+	 * to this screenobject.
+	*/
+	void refresh()
+	{
+		refreshCDKScreen(_ptr.get());
+	}
 	/**
 	 * @brief lowerObject.
 	 * Has the opposite effect of the raiseCDKObject function call.
@@ -135,16 +144,17 @@ public:
 
 struct point
 {
-    int x,y;
+	int x,y;
 };
 
 struct drawing_options
 {
-    bool box{false},shadow{false};
+	bool box{false},shadow{false};
 };
 
-struct move_options {
-    bool relative{false},refresh{false};
+struct move_options
+{
+	bool relative{false},refresh{false};
 };
 
 /**
@@ -154,196 +164,199 @@ class label :public widget
 {
 	struct deleter
 	{
-		void operator()(CDKLABEL* p) {destroyCDKLabel(p);}
+		void operator()(CDKLABEL* p)
+		{
+			destroyCDKLabel(p);
+		}
 	};
 	using labelptr = std::unique_ptr<CDKLABEL,deleter>;
-    labelptr _ptr;
+	labelptr _ptr;
 public:
-    /**
-     * Default constructor.
-     * Creates an empty object, dont call
-     * member functions without assigning
-     * an initialized object
-    */
-    label() = default;
+	/**
+	 * Default constructor.
+	 * Creates an empty object, dont call
+	 * member functions without assigning
+	 * an initialized object
+	*/
+	label() = default;
 
-    label(screen& parent,point p,StringList message, drawing_options opt)
-    {
+	label(screen& parent,point p,StringList message, drawing_options opt)
+	{
 		auto v = transformStringList(message);
 		_ptr = labelptr(newCDKLabel(parent._ptr.get(),
-                    p.x,p.y,
-		            v.data(),v.size(),
-		            opt.box,opt.shadow));
+									p.x,p.y,
+									v.data(),v.size(),
+									opt.box,opt.shadow));
 		_vptr = _ptr.get();
-    }
-    /**
-     * Draws the label widget on the screen.  If the box parameter is true, the widget is drawn with a box.
-    */
+	}
+	/**
+	 * Draws the label widget on the screen.  If the box parameter is true, the widget is drawn with a box.
+	*/
 	void draw(boolean box=false)
-    {
+	{
 		drawCDKLabel(_ptr.get(),box);
-    }
-    /**
-     * Removes the widget from the screen.  This does NOT destroy the widget.
-    */
+	}
+	/**
+	 * Removes the widget from the screen.  This does NOT destroy the widget.
+	*/
 	void erase()
-    {
-        eraseCDKLabel(_ptr.get());
-    }
-    /**
-     * Returns true if the widget will be drawn with a box around it.
-    */
-    bool getBox()
-    {
-        return getCDKLabelBox(_ptr.get());
-    }
-    /**
-     * Returns the contents of the label widget.  
-     * The parameter messageLines points to the location 
-     * which will be given the number of message-lines.
-    */
-    StringList getMessage()
-    {
-        StringList v;
-        int linesCount=0;
-        auto msg = getCDKLabelMessage(_ptr.get(),&linesCount);
-        for(int i = 0 ; i< linesCount;++i)
-        {
+	{
+		eraseCDKLabel(_ptr.get());
+	}
+	/**
+	 * Returns true if the widget will be drawn with a box around it.
+	*/
+	bool getBox()
+	{
+		return getCDKLabelBox(_ptr.get());
+	}
+	/**
+	 * Returns the contents of the label widget.
+	 * The parameter messageLines points to the location
+	 * which will be given the number of message-lines.
+	*/
+	StringList getMessage()
+	{
+		StringList v;
+		int linesCount=0;
+		auto msg = getCDKLabelMessage(_ptr.get(),&linesCount);
+		for(int i = 0 ; i< linesCount; ++i)
+		{
 			v.push_back(reinterpret_cast<char*>(msg[i]));
-        }
-        return v;
-    }
-    /**
-     * moves the widget to the given point.
+		}
+		return v;
+	}
+	/**
+	 * moves the widget to the given point.
 
-            •   The parameters p.x and p.y are the new position of the widget.
+	        •   The parameters p.x and p.y are the new position of the widget.
 
-                The parameter p.x may be an integer or one of the pre-defined values TOP, BOTTOM, and CENTER.
+	            The parameter p.x may be an integer or one of the pre-defined values TOP, BOTTOM, and CENTER.
 
-                The parameter p.y may be an integer or one of the pre-defined values LEFT, RIGHT, and CENTER.
+	            The parameter p.y may be an integer or one of the pre-defined values LEFT, RIGHT, and CENTER.
 
-            •   The parameter relative states whether the p.x/p.y pair is a relative move or an absolute move.
-    */
-    void move(point p,move_options o)
-    {
-        moveCDKLabel(_ptr.get(),
-                p.x,p.y,
-                o.relative,o.refresh);
-    }
-    /**
-     * Allows the user to move the widget around the screen 
-     * via the cursor/keypad keys.  
-     * See cdk_position (3) for key bindings.
-    */
-    void position()
-    {
-        positionCDKLabel(_ptr.get());
-    }
-    /**
-     * allows the user to change the contents of the label widget.  
-     * The parameters are the same as the newCDKLabel.
-    */
-    void set(StringList message,bool box)
-    {
-        auto v = transformStringList(message);
-        setCDKLabel(_ptr.get(),
-		    v.data(),v.size(),
-            box);
-    }
-    /**
-     * Sets the background attribute of the widget.  
-     * The parameter attribute is a curses attribute, 
-     * e.g., A_BOLD.
-    */
-    void setBackgroundAttrib(chtype attribute)
-    {
-        setCDKLabelBackgroundAttrib(_ptr.get(),attribute);
-    }
-    /**
-     * Sets the background color of the widget.  
-     * The parameter color is in the format of 
-     * the Cdk format strings.  
-     * For more information see cdk_display (3).
-    */
-    void setBackgroundColor(const char * color)
-    {
+	        •   The parameter relative states whether the p.x/p.y pair is a relative move or an absolute move.
+	*/
+	void move(point p,move_options o)
+	{
+		moveCDKLabel(_ptr.get(),
+					 p.x,p.y,
+					 o.relative,o.refresh);
+	}
+	/**
+	 * Allows the user to move the widget around the screen
+	 * via the cursor/keypad keys.
+	 * See cdk_position (3) for key bindings.
+	*/
+	void position()
+	{
+		positionCDKLabel(_ptr.get());
+	}
+	/**
+	 * allows the user to change the contents of the label widget.
+	 * The parameters are the same as the newCDKLabel.
+	*/
+	void set(StringList message,bool box)
+	{
+		auto v = transformStringList(message);
+		setCDKLabel(_ptr.get(),
+					v.data(),v.size(),
+					box);
+	}
+	/**
+	 * Sets the background attribute of the widget.
+	 * The parameter attribute is a curses attribute,
+	 * e.g., A_BOLD.
+	*/
+	void setBackgroundAttrib(chtype attribute)
+	{
+		setCDKLabelBackgroundAttrib(_ptr.get(),attribute);
+	}
+	/**
+	 * Sets the background color of the widget.
+	 * The parameter color is in the format of
+	 * the Cdk format strings.
+	 * For more information see cdk_display (3).
+	*/
+	void setBackgroundColor(const char * color)
+	{
 		setCDKLabelBackgroundColor(_ptr.get(),color);
-    }
-    /**
-     * Sets whether the widget will be drawn with a box around it.
-    */
-    void setBox(bool box)
-    {
-        setCDKLabelBox(_ptr.get(),box);
-    }
-    /**
-     * Sets the attribute of the box.
-    */
-    void setBoxAttribute(chtype character)
-    {
-        setCDKLabelBoxAttribute(_ptr.get(),character);
-    }
-    /**
-     * Sets the horizontal drawing character for the box to the given character.
-    */
-    void setHorizontalChar(chtype character)
-    {
-        setCDKLabelHorizontalChar(_ptr.get(),character);
-    }
-    /**
-     * Sets the lower left hand corner of the widget's box to the given character.
-    */
-    void setLLChar(chtype character)
-    {
-        setCDKLabelLLChar(_ptr.get(),character);
-    }
-    /**
-     * Sets the lower right hand corner of the widget's box to the given character.
-    */
-    void setLRChar(chtype character)
-    {
+	}
+	/**
+	 * Sets whether the widget will be drawn with a box around it.
+	*/
+	void setBox(bool box)
+	{
+		setCDKLabelBox(_ptr.get(),box);
+	}
+	/**
+	 * Sets the attribute of the box.
+	*/
+	void setBoxAttribute(chtype character)
+	{
+		setCDKLabelBoxAttribute(_ptr.get(),character);
+	}
+	/**
+	 * Sets the horizontal drawing character for the box to the given character.
+	*/
+	void setHorizontalChar(chtype character)
+	{
+		setCDKLabelHorizontalChar(_ptr.get(),character);
+	}
+	/**
+	 * Sets the lower left hand corner of the widget's box to the given character.
+	*/
+	void setLLChar(chtype character)
+	{
+		setCDKLabelLLChar(_ptr.get(),character);
+	}
+	/**
+	 * Sets the lower right hand corner of the widget's box to the given character.
+	*/
+	void setLRChar(chtype character)
+	{
 		setCDKLabelLRChar(_ptr.get(),character);
-    }
-    /**
-     * This sets the contents of the label widget.
-    */
-    void setMessage(StringList message)
-    {
-        auto v = transformStringList(message);
-        setCDKLabelMessage(_ptr.get(),
-            v.data(),v.size());
-    }
-    /**
-     * Sets the upper left hand corner of the widget's box to the given character.
-    */
-    void setULChar(chtype character)
-    {
-        setCDKLabelULChar(_ptr.get(),character);
-    }
-    /**
-     * Sets the upper right hand corner of the widget's box to the given character.
-    */
-    void setURChar(chtype character)
-    {
-        setCDKLabelURChar(_ptr.get(),character);
-    }
-    /**
-     * Sets the vertical drawing character for the box to the given character.
-    */
-    void setVerticalChar(chtype character)
-    {
-        setCDKLabelVerticalChar(_ptr.get(),character);
-    }
-    /**
-     * Waits for a user to press a key.  
-     * The label parameter is the pointer to a label widget, 
-     * and key is the key to wait for.  
-     * If no specific key is desired, use (char)0.
-    */
-    char wait(char key)
-    {
+	}
+	/**
+	 * This sets the contents of the label widget.
+	*/
+	void setMessage(StringList message)
+	{
+		auto v = transformStringList(message);
+		setCDKLabelMessage(_ptr.get(),
+						   v.data(),v.size());
+	}
+	/**
+	 * Sets the upper left hand corner of the widget's box to the given character.
+	*/
+	void setULChar(chtype character)
+	{
+		setCDKLabelULChar(_ptr.get(),character);
+	}
+	/**
+	 * Sets the upper right hand corner of the widget's box to the given character.
+	*/
+	void setURChar(chtype character)
+	{
+		setCDKLabelURChar(_ptr.get(),character);
+	}
+	/**
+	 * Sets the vertical drawing character for the box to the given character.
+	*/
+	void setVerticalChar(chtype character)
+	{
+		setCDKLabelVerticalChar(_ptr.get(),character);
+	}
+	/**
+	 * Waits for a user to press a key.
+	 * The label parameter is the pointer to a label widget,
+	 * and key is the key to wait for.
+	 * If no specific key is desired, use (char)0.
+	*/
+	char wait(char key)
+	{
 		return waitCDKLabel(_ptr.get(),key);
-    }
+	}
 	EObjectType type{vLABEL};
 
 };
@@ -352,7 +365,10 @@ class button : public widget
 {
 	struct deleter
 	{
-		void operator()(CDKBUTTON* p) {destroyCDKButton(p);}
+		void operator()(CDKBUTTON* p)
+		{
+			destroyCDKButton(p);
+		}
 	};
 	using buttonptr = std::unique_ptr<CDKBUTTON,deleter>;
 	buttonptr _ptr;
@@ -368,10 +384,10 @@ public:
 	button(screen& s,point p, std::string_view message,tButtonCallback cb,drawing_options o)
 	{
 		_ptr = buttonptr(newCDKButton(s._ptr.get(),
-		             p.x,p.y,
-		             message.data(),
-		             cb,
-		             o.box,o.shadow));
+									  p.x,p.y,
+									  message.data(),
+									  cb,
+									  o.box,o.shadow));
 		_vptr = _ptr.get();
 	}
 	int activate(chtype * actions)
@@ -450,8 +466,8 @@ public:
 	void move(point p,move_options o)
 	{
 		moveCDKButton(_ptr.get(),
-		                p.x,p.y,
-		                o.relative,o.refresh);
+					  p.x,p.y,
+					  o.relative,o.refresh);
 	}
 	void position()
 	{
--- demo/demo.cpp
+++ demo/demo.cpp
@@ -1,13 +1,13 @@
-
 #include "../cdk.hpp"
 #include <iostream>
+
 int main()
 {
-    cdk::screen screen(initscr());
+	cdk::screen screen(initscr());
 	cdk::label label;
-	label = cdk::label(screen,{CENTER,5},{"libcdkpp","demo","1.0"},{});
-	cdk::button button(screen,{CENTER,10},"Press me",NULL,{true,true});
+	label = cdk::label(screen, {CENTER,5}, {"libcdkpp","demo","1.0"}, {});
+	cdk::button button(screen, {CENTER,10},"Press me",NULL, {true,true});
 	screen.refresh();
 	button.activate(NULL);
-    return 0;
+	return 0;
 }
